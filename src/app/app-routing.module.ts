c2c3eca99414f85dae9cb5d893133c75b2644f92
croci <dorian.croci@edu.ece.fr>
2024-04-04 10:43:12 +0200

    commit pour réparer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { LoginComponent } from "login/login.component"
 import { InscriptionComponent } from "inscription/inscription.component"
 import { ProfilComponent } from "profil/profil.component"
 
+
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "login", component: LoginComponent },
