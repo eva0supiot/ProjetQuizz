0b963511771c7e49e8d3cfa74cf3ff3ab6dacccb
croci <dorian.croci@edu.ece.fr>
2024-05-03 12:07:44 +0200

    Inscription fonctionnelle avec la BDD

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from "@angular/router"
 import {Observable, of} from "rxjs";
 import {MatRadioButton, MatRadioChange, MatRadioGroup} from "@angular/material/radio";
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatButton} from "@angular/material/button";
 import {MatCard, MatCardHeader, MatCardSmImage, MatCardTitle, MatCardTitleGroup} from "@angular/material/card";
+import { Utilisateur } from "../models/utilisateur.model"
+import { UtilisateurService } from "../services/utilisateur.service"
+import { QuizzService } from "../services/quizz.service"
+import { QuestionService } from "../services/question.service"
+import { MatSnackBar } from "@angular/material/snack-bar"
+import { AuthService } from "../services/auth.service"
 
 @Component({
   selector: 'app-root',
@@ -15,27 +21,31 @@ import {MatCard, MatCardHeader, MatCardSmImage, MatCardTitle, MatCardTitleGroup}
   styleUrl: './inscription.component.scss'
 })
 export class InscriptionComponent {
-  users : User [] = []
+  utilisateurs: Utilisateur [] = []
+
+  userForm: FormGroup = new FormGroup({});
 
   // il existe deux facons de faire des formulaires en angular : ici les reactives forms (sinon c'est ngForm)
-  myForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    imageName: new FormControl('', Validators.required)
-  });
-  constructor() {
-    this.mockUserData().subscribe((users => this.users = users));
+  initForm() {
+    this.userForm = this.fb.group({
+      pseudo: ['', Validators.required],
+      mdp: ['', Validators.required],
+      admin: [false, Validators.required],
+      scores: ['0', Validators.required],
+      pdp: [null]
+    });
   }
 
-  // cette méthode mock le post au back
-  postUser(user : User) {
-    this.users.push(user);
+
+
+  constructor(private _route: ActivatedRoute, private utilisateurService: UtilisateurService, private router: Router, private fb: FormBuilder, private _snackBar: MatSnackBar,private authService: AuthService) {
+    this.utilisateurService.findAll().subscribe((data) => this.utilisateurs = data)
+    this.initForm();
+
   }
 
-  // cette méthode sert à faire comme si j'appelais mon API et qu'elle me retournais deux utilisateurs
-  mockUserData() : Observable<User[]> {
-    const users : User[] = [{id: "AZER", imageName: "image1"}, {id: "TFES", imageName: "image2"}]
-    return of(users)
-  }
+
+
 
   // cette méthode n'est pas obligatoire c'est pour montrer comment récupérer la valeur d'un radio button
   onRadioButtonChange($event: MatRadioChange) {
@@ -45,10 +55,27 @@ export class InscriptionComponent {
 
   // lorsque je soumets mon formulaire
   onSubmit() {
-    console.log(this.myForm)
-    const newUser: User = {id: this.myForm.controls.name.value!!, imageName : this.myForm.controls.imageName.value!!}
-    console.log(newUser)
-    this.postUser(newUser);
+    const pseudoExist = this.utilisateurs.some(user => user.pseudo === this.userForm.value.pseudo);
+    if (pseudoExist) {
+      this._snackBar.open('L\'utilisateur existe déjà', '', { duration: 5000 });
+      return;
+    } else {
+      const newUser: Utilisateur = this.userForm.value;
+
+      this.utilisateurService.add(newUser).subscribe(
+        response => {
+          this._snackBar.open('Utilisateur ajouté avec succès', '', { duration: 5000 });
+          this.authService.login(newUser);
+          this.router.navigate([newUser.admin ? 'profil-admin' : 'profil'], { queryParams: { username: newUser.pseudo } });
+          console.log('Utilisateur ajouté avec succès', response);
+          this.userForm.reset();
+        },
+        error => {
+          this._snackBar.open('Erreur lors de l\'ajout de l\'utilisateur', '', { duration: 5000 });
+          console.error('Erreur lors de l\'ajout de l\'utilisateur', error);
+        }
+      );
+    }
   }
 }
 
